b2a56dff628a7bf229243f0ab73b6472ba0ef9d8
xXAMIRAYA <xXAMIRAYA@github.com>
2025-05-22 11:38:00 +0100

    merge to main

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+    build: {
+    chunkSizeWarningLimit: 1500, // augmente la limite à 1.5 Mo
+  },
   server: {
     host: "::",
     port: 8080,
